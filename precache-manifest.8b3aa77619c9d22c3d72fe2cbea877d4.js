8f8bace75dba849afad8a72cb34bb1d3da7fc27a
rim-code <friouirim@gmail.com>
2019-11-25 11:02:35 +0100

    Updates

---
+++ precache-manifest.8b3aa77619c9d22c3d72fe2cbea877d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0699186673625b68eae1639c242ce85f",
+    "url": "/navmenu/index.html"
+  },
+  {
+    "revision": "71a15624f7a00bfb7b1f",
+    "url": "/navmenu/static/css/main.77132c61.chunk.css"
+  },
+  {
+    "revision": "3267ca8c1d64bc19a702",
+    "url": "/navmenu/static/js/2.9b60dd3f.chunk.js"
+  },
+  {
+    "revision": "71a15624f7a00bfb7b1f",
+    "url": "/navmenu/static/js/main.3b767815.chunk.js"
+  },
+  {
+    "revision": "079ed0dfdaf257d7bac7",
+    "url": "/navmenu/static/js/runtime-main.59c0c123.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/navmenu/static/media/logo.25bf045c.svg"
+  }
+]);
